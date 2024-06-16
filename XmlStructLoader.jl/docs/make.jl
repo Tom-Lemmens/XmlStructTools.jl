aba3975f0cf9fc19a7811b8da1e174e3019488f3
Tom Lemmens <lemmens_tom@skynet.be>
2024-06-16 13:20:54 +0200

    Update doc make.jl

--- XmlStructLoader.jl/docs/make.jl
+++ XmlStructLoader.jl/docs/make.jl
@@ -1,15 +1,9 @@
 using XmlStructLoader
 using Documenter
 
-DocMeta.setdocmeta!(XmlStructLoader, :DocTestSetup, :(using XmlStructLoader); recursive = true)
-
 makedocs(;
     modules = [XmlStructLoader],
-    authors = "ASML",
-    repo = "https://github.com/ASML-Labs/XmlStructTools.jl/XmlStructLoader.jl/blob/{commit}{path}#{line}",
+    repo = Remotes.GitHub("Tom-Lemmens", "XmlStructTools.jl"),
     sitename = "XmlStructLoader.jl",
-    format = Documenter.HTML(; prettyurls = get(ENV, "CI", "false") == "true", edit_link = "main", assets = String[]),
     pages = ["Home" => "index.md", "Docstrings" => "docstrings.md"],
 )
-
-deploydocs(repo = "github.com/ASML-Labs/XmlStructTools.jl/XmlStructLoader.jl.git")
